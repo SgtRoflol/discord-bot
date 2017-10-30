552f9566152d0fdd315df377bb986705d25d6d03
supertimbox <tim.feucht@icloud.com>
2017-10-30 21:16:31 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,109 @@
+const Discord = require("discord.js"); // require discord.js
+const bot = new Discord.Client(); // create the discord client
+const client = bot
+const token = "MzcyMTA1MzY5NDIzNTc3MDg5.DM_Vqw.TOdFGhi4-hftPlv5jV7wbxuPU8I"
+; // store the token in a variable
+const prefix = "!"
+bot.on("ready", function() {
+  console.log("ready");
+});
+bot.on("guildMemberAdd", function(member) {
+  member.guild.channels.find("name", "general").sendMessage(member.toString() + " hi o/ this server is cool plz no leave");
+  member.addRole(member.guild.roles.find("name", "mortal"));
+});
+bot.on("message",function(message) {
+  if (message.author.equals(bot.user)) return;
+
+  if (!message.content.startsWith(prefix)) return;
+
+  var args = message.content.substring(prefix.length).split(" ");
+
+  switch (args[0].toLowerCase()) {
+    case "ping":
+    message.channel.send("pong!");
+    break;
+
+    case"space":
+var time = Date.now();
+message.delete();
+    if (message.member.roles.has("371668187957166082")) {
+      message.channel.send("spaaace",{file: "https://i.pinimg.com/originals/a9/ef/38/a9ef38cdc7125701e1c939e10489c7b3.gif"})
+             .then(msg=>msg.edit(`${msg.contents} took ${Date.now() - time}`));
+}
+else {
+  message.reply("DENIED!")
+}
+      break;
+
+      case "help":
+      message.author.send("commands:");
+      message.author.send("`time` displays time it took to send the message");
+      message.author.send("`test` testmessage");
+      message.author.send("`info` an info embed");
+      message.author.send("`goodnight` goodnight");
+      message.delete();
+      break;
+
+      case "hentai":
+      message.delete();
+      if (message.member.roles.has("371668187957166082")){
+      message.channel.send("https://www.youtube.com/watch?v=DBz2_FTeTKc&feature=youtu.be");
+    }
+    else {
+      message.channel.send("DENIED");
+    }
+      break;
+
+
+    case"test":
+    message.delete();
+    message.reply("henlo o/");
+    break;
+
+    case"info":
+    message.channel.send({embed: {
+      color: 0xffffff,
+      author : {
+        name: "roflbot",
+    icon_url: client.user.avatarURL
+  },
+  title:"Info",
+  description:"Roflbot is a bot made by Supertimbox",
+  timestamp: new Date(),
+  footer: {
+    icon_url: client.user.avatarURL,
+    text:"©Supertimbox"
+  }
+    }})
+    break;
+
+    case"time":
+    message.delete();
+    var time = Date.now();
+    message.channel.send("time")
+    .then(msg=>msg.edit(msg + ' took ' + time));
+    break;
+
+    case"goodnight":
+    message.delete();
+    message.channel.send("gnight o/");
+    break;
+
+    case "lol":
+    message.delete();
+    message.channel.send("lol");
+    break;
+
+    case"weeb":
+    message.author.send("https://www.youtube.com/watch?v=DBz2_FTeTKc&feature=youtu.be");
+    message.author.send("fucking weeb -.-");
+    message.delete();
+    break;
+
+
+    default:
+    message.channel.send("invalid command");
+  }
+});
+
+bot.login(token);
