c98fd3dda08709b7f5b04ed7fc0e526aee20785c
supertimbox <33234688+supertimbox@users.noreply.github.com>
2018-03-16 21:15:51 +0100

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ bot.on("message",function(message) {
 
   if (!message.content.startsWith(prefix)) return;
 
-
+client.user.setPresence({ game: { name: './help for help' });
+  
   var args = message.content.substring(prefix.length).split(" ");
   
   switch (args[0].toLowerCase()) {
