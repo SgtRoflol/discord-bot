4d8788387631a25e05c24250dedde9148147ee47
supertimbox <33234688+supertimbox@users.noreply.github.com>
2018-03-16 17:54:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -17,8 +17,7 @@ bot.on("message",function(message) {
 
 
   var args = message.content.substring(prefix.length).split(" ");
-  
-  client.setPlayingGame("./help for help")
+
   
   switch (args[0].toLowerCase()) {
     case "ping":
